181195669c5982802fe93aee34252d69bbe6b610
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-11-07 21:02:37 -0800

    with bonus

--- BasicCard.js
+++ BasicCard.js
@@ -1,6 +1,11 @@
 function BasicCard(front, back) {
-  this.front  = front;
-  this.back   = back;
+  if (this instanceof BasicCard) {
+    this.front  = front;
+    this.back   = back;
+  }
+  else {
+    return new BasicCard(front, back);
+  }
 }
 
 module.exports = BasicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,22 @@
-function ClozeCard(obj) {
-  this.cloze    = obj.cloze;
-  this.partial  = obj.partial;
-  this.fullText = obj.fullText;
+function ClozeCard(text, cloze) {
+  if (this instanceof ClozeCard) {
+    this.cloze    = cloze;
+    this.fullText = text;
+    this.partial;
+
+    var re = new RegExp(cloze, 'g');
+
+    if (text.indexOf(cloze) === -1) {
+      console.log('ERROR');
+      this.partial = -1;
+    }
+    else {
+      this.partial = text.replace(re, '...');
+    }
+  }
+  else {
+    return new ClozeCard(text, cloze);
+  }
 
   /*
   * The constructor should throw or log an error when 
@@ -10,5 +25,16 @@ function ClozeCard(obj) {
 
   //* Use prototypes to attach these methods, wherever possible.
 }
+// ClozeCard.prototype.partial = setPartial(this.fullText, this.cloze);
+
+// function setPartial(text, cloze) {
+//   if (text.indexOf(cloze) === -1) {
+//     throw new Error("ERROR!!");
+//   }
+//   else {
+//     var re = new RegExp(this.cloze, 'g');
+//     return text.replace(re, '...');
+//   }
+// }
 
 module.exports = ClozeCard;
