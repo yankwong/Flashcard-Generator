894f96c36accb207a097632403ac7eecb81a3121
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-11-07 21:19:37 -0800

    removed comment

--- BasicCard.js
+++ BasicCard.js
@@ -9,8 +9,3 @@ function BasicCard(front, back) {
 }
 
 module.exports = BasicCard;
-
-/*
-* Write your constructors such that users can call them with or without the `new` keyword. 
-* Look up scope-safe constructors, and try to implement them. It takes only two additional lines of code.
-*/
