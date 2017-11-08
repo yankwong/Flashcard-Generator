fbd1dcd640756c52a21cd4c7fabc2a64fff307f7
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-11-07 19:32:46 -0800

    init commit

---
+++ BasicCard.js
@@ -0,0 +1,11 @@
+function BasicCard(front, back) {
+  this.front  = front;
+  this.back   = back;
+}
+
+module.exports = BasicCard;
+
+/*
+* Write your constructors such that users can call them with or without the `new` keyword. 
+* Look up scope-safe constructors, and try to implement them. It takes only two additional lines of code.
+*/
---
+++ ClozeCard.js
@@ -0,0 +1,14 @@
+function ClozeCard(obj) {
+  this.cloze    = obj.cloze;
+  this.partial  = obj.partial;
+  this.fullText = obj.fullText;
+
+  /*
+  * The constructor should throw or log an error when 
+  * the cloze deletion does _not_ appear in the input text.
+  */
+
+  //* Use prototypes to attach these methods, wherever possible.
+}
+
+module.exports = ClozeCard;
